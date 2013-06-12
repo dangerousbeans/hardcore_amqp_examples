cd42a353094eeba710988525a2768e3fbb7126ff
Joran Kikke <joran.k@gmail.com>
2013-06-12 15:54:45 +0100

    working example

--- request_reply/client.rb
+++ request_reply/client.rb
@@ -1,27 +1,23 @@
-require "rubygems"
 require "amqp"
 
 EventMachine.run do
-  connection = AMQP.connect(:host => '127.0.0.1')
-  channel  = AMQP::Channel.new(connection)
-  low_queue    = channel.queue("low")
-  high_queue    = channel.queue("high")
-  exchange = channel.direct("")
+  connection = AMQP.connect
+  channel    = AMQP::Channel.new(connection)
 
-
-  10.times do |i| 
-    message = "LOW #{i}"
-    puts "sending: #{message}"
-    exchange.publish message, :routing_key => low_queue.name
+  replies_queue = channel.queue("", :exclusive => true, :auto_delete => true)
+  replies_queue.subscribe do |metadata, payload|
+    puts "[response] Response for #{metadata.correlation_id}: #{payload.inspect}"
   end
 
-  # EventMachine.add_periodic_timer(0.0001) do
-  10.times do |i|
-    message = "HIGH #{i}"
-    puts "sending: #{message}"
-    exchange.publish message, :routing_key => high_queue.name
+  # request time from a peer every 3 seconds
+  EventMachine.add_periodic_timer(3.0) do
+    puts "[request] Sending a request..."
+    channel.default_exchange.publish("get.time",
+                                     :routing_key => "amqpgem.examples.services.time",
+                                     :message_id  => Kernel.rand(10101010).to_s,
+                                     :reply_to    => replies_queue.name)
   end
-  # end
-   
-  # connection.close { EventMachine.stop } 
+
+
+  Signal.trap("INT") { connection.close { EventMachine.stop } }
 end
