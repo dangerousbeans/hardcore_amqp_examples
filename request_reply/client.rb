1e6be2a44f2b77a20ce308583c79c47d241c8e28
Joran Kikke <joran.k@gmail.com>
2013-06-12 15:56:54 +0100

    nicer example

--- request_reply/client.rb
+++ request_reply/client.rb
@@ -6,14 +6,14 @@ EventMachine.run do
 
   replies_queue = channel.queue("", :exclusive => true, :auto_delete => true)
   replies_queue.subscribe do |metadata, payload|
-    puts "[response] Response for #{metadata.correlation_id}: #{payload.inspect}"
+    puts "Reply: #{payload.inspect}"
   end
 
   # request time from a peer every 3 seconds
   EventMachine.add_periodic_timer(3.0) do
     puts "[request] Sending a request..."
-    channel.default_exchange.publish("get.time",
-                                     :routing_key => "amqpgem.examples.services.time",
+    channel.default_exchange.publish("Hello?",
+                                     :routing_key => "test.messages",
                                      :message_id  => Kernel.rand(10101010).to_s,
                                      :reply_to    => replies_queue.name)
   end
--- request_reply/server.rb
+++ request_reply/server.rb
@@ -4,10 +4,10 @@ EventMachine.run do
   connection = AMQP.connect
   channel    = AMQP::Channel.new(connection)
 
-  requests_queue = channel.queue("amqpgem.examples.services.time", :exclusive => true, :auto_delete => true)
+  requests_queue = channel.queue("test.messages", :exclusive => true, :auto_delete => true)
   requests_queue.subscribe(:ack => true) do |metadata, payload|
     puts "[requests] Got a request #{metadata.message_id}. Sending a reply..."
-    channel.default_exchange.publish(Time.now.to_s,
+    channel.default_exchange.publish("HAI THUR :D",
                                      :routing_key    => metadata.reply_to,
                                      :correlation_id => metadata.message_id,
                                      :mandatory      => true)
